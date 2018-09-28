b40cc230528679b0cc52ed432519449da650ca5b
mstrahlert <magnus.strahlert@gmail.com>
2018-09-28 14:35:07 +0200

    Sorted the output on title instead of the default sort on id

--- list_vms.py
+++ list_vms.py
@@ -25,21 +25,24 @@ def main():
   _memtot=0
   _cputot=0
 
-  for vm in guest_vms.splitlines():
-    vm = vm.split()
-    if len(vm) == False or len(vm) < 3 or vm[0] == "Id":
+  arr = {}
+  for (i, vm) in enumerate(guest_vms.splitlines()):
+    if len(vm.split()) == False or len(vm.split()) < 3 or vm.split()[0] == "Id":
       continue
 
-    xmlroot = ET.fromstring(cmdline("virsh dumpxml %s" % vm[1])) 
+    arr[i] = vm.split()
+
+  for vm in sorted(arr.items(), key=lambda item: item[1][1]):
+    xmlroot = ET.fromstring(cmdline("virsh dumpxml %s" % vm[1][1]))
     _mem = xmlroot.findtext("memory")
     _cpu = xmlroot.findtext("vcpu")
     _title = xmlroot.findtext("title")
     _memtot += int(_mem)
     _cputot += int(_cpu)
 
-    print("%35s: %.2fGb, %s vcpu, %s" % ((truncate(_title, 34) or vm[1]),
+    print("%35s: %.2fGb, %s vcpu, %s" % ((truncate(_title, 34) or vm[1][1]),
                                          int(_mem) / 1024 / 1024., _cpu,
-                                         " ".join(vm[2:])))
+                                         " ".join(vm[1][2:])))
 
   print("%35s: %.2fGb, %s vcpu" % ("** Total **", int(_memtot) / 1024 / 1024.,
                                    _cputot))
